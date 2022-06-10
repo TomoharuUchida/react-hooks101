700a782066932ec6c6ee67b0763d040945cd9a7c
TomoharuUchida <tomou0711@gmail.com>
2022-06-10 15:10:28 +0900

    display events, refactor and implement event button

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React, { useReducer,useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import reducer from '../reducers';
+import Event from './Event'
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
@@ -48,6 +49,7 @@ const App = () => {
           </tr>
           </thead>
           <tbody>
+            {state.map((event, index) =>  (<Event key={index} event={event} dispatch={dispatch}/>))}
           </tbody>
         </table>
       </form>
