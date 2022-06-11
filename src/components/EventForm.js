a66afdac9c36032f7c6f4b34d108737cdd531051
TomoharuUchida <tomou0711@gmail.com>
2022-06-11 13:37:14 +0900

    create operation logs

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -2,9 +2,12 @@ import React,{ useContext,useState } from 'react';
 
 import {
     CREATE_EVENT,
-    EDELETE_ALL_EVENTS
+    EDELETE_ALL_EVENTS,
+    ADD_OPERATION_LOG,
+    DELETE_ALL_OPERATION_LOGS
 } from '../actions'
 import AppContext from '../contexts/AppContext'
+import { timeCurrentIso8601 } from '../utils';
 
 const EventForm = () => {
     const {state,dispatch} = useContext(AppContext)
@@ -14,9 +17,14 @@ const EventForm = () => {
     const addEvent = (e) => {
         e.preventDefault();
         dispatch({
-        type: CREATE_EVENT,
-        title,
-        body
+            type: CREATE_EVENT,
+            title,
+            body
+        })
+        dispatch({
+            type: ADD_OPERATION_LOG,
+            description: 'added an event',
+            operatedAt:timeCurrentIso8601()
         })
         setTitle('')
         setBody('')
@@ -25,7 +33,15 @@ const EventForm = () => {
     const deleteAllEvents = (e) => {
         e.preventDefault()
         const result = window.confirm('Are you sure you want to delete all events?')
-        if(result)dispatch({ type: EDELETE_ALL_EVENTS})
+        if (result) {
+            dispatch({ type: EDELETE_ALL_EVENTS })
+            
+            dispatch({
+                type: ADD_OPERATION_LOG,
+                description: 'deleted all events',
+                operatedAt:timeCurrentIso8601()
+            })
+         } 
     }
 
     const unCreatable = title === '' || body === ''
